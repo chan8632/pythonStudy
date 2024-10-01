7a3349e03fafa0319e6c6d5ddafd9a5acd1e5969
chan8632 <duscks8632@gmail.com>
2024-10-01 11:23:13 +0900

    [다이나믹프로그래밍]1로 만들기 정답

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 x = int(input())
-d = [0] * x
+d = [0] * (x + 1)
 
 
 def cal(x):
-    for i in range(2, x):
+    for i in range(2, x + 1):
         d[i] = d[i - 1] + 1
         if i % 2 == 0:
-            min(d[i], d[i // 2] + 1)
+            d[i] = min(d[i], d[i // 2] + 1)
         if i % 3 == 0:
-            min(d[i], d[i // 3] + 1)
+            d[i] = min(d[i], d[i // 3] + 1)
         if i % 5 == 0:
-            min(d[i], d[i // 5] + 1)
+            d[i] = min(d[i], d[i // 5] + 1)
     return d[x]
 
 
