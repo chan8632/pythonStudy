bd6f818ea47379c890eea36499e1eaad16ad3ac3
chan8632 <duscks8632@gmail.com>
2024-10-07 11:50:48 +0900

    [다이나믹 프로그래밍] 개미전사

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 x = int(input())
-d = [0] * (x + 1)
+array = list(map(int, input().split()))
 
+# dp 테이블 100까지 설정이 맞는지?
+# 맞다. 인덱스 값이 아니라 그냥 메모리 100개를 만드는 거기에
+dp = [0] * 100
+dp[0] = array[0]
+dp[1] = max(array[0], array[1])
 
-def one(x):
-    for i in range(2, x + 1):
-        d[i] = d[i - 1] + 1
-        if i % 2 == 0:
-            d[i] = min(d[i], d[i // 2] + 1)
-        if i % 3 == 0:
-            d[i] = min(d[i], d[i // 3] + 1)
-        if i % 5 == 0:
-            d[i] = min(d[i], d[i // 5] + 1)
-    return d[i]
+# for문 x까지 범위가 맞는지? x+1인가? x로 하고 마지막에 x-1를 출력하면 된다.
+# return  안해도 되는가? 네 함수가 아니기에
+for i in range(2, x):
+    dp[i] = max(dp[i - 1], dp[i - 2] + array[i])
 
-
-print(one(x))
+print(dp[x - 1])
